8482ec6f98d59b8d522e9fe5459c2afa5581ccc0
Aishwaryadjadhav <aishwaryadj1@gmail.com>
2024-12-17 15:36:58 +0530

    create login page

---
+++ flipkart.js
@@ -0,0 +1,30 @@
+// function toggleHide()
+// {
+//     let div3 = document.getElementsByClassName('div3')
+//     let seconddiv=document.getElementsByClassName('seconddiv')
+//     seconddiv.style.display='none'
+// }
+
+function toggleHide()
+{
+    let a = document.getElementById('aishu')
+    let b = document.getElementById('demo')
+    if(b.style.display!='block')
+    {
+        b.style.display='block'
+    }
+    else{
+        b.style.display='none'
+    }
+    
+}
+
+
+// function hide()
+// {
+//     let c = document.getElementById('demo')
+//     c.style.display='none'
+// }
+
+
+
